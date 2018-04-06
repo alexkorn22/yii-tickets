46ef78b4066212193a4ea35dce9880ac0415ab6b
Aleksandr <a@test.com>
2018-04-06 20:51:20 +0300

    Исправлены миграции

--- www/migrations/m180210_112629_add_position_column_to_user_table.php
+++ www/migrations/m180210_112629_add_position_column_to_user_table.php
@@ -13,6 +13,15 @@ class m180210_112629_add_position_column_to_user_table extends Migration
     public function up()
     {
         $this->addColumn('user', 'guid', $this->string(255));
+        $data = [
+            'login' => 'admin',
+            'password' => Yii::$app->getSecurity()->generatePasswordHash('admin'),
+            'name' => 'admin',
+            'second_name' => 'admin',
+            'is_admin' => 1,
+            'guid' => 'bbf8b678-a569-11e3-925b-82d70ac539aa'
+        ];
+        $this->insert('user', $data);
     }
 
     /**
